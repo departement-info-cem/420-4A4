0d0b212a8db52a175db7bf779b5e9682a9aa0552
glorotx <xavier.glorot@cegepmontpetit.ca>
2025-08-24 18:50:05 -0400

    Commit initial

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
-    nom: "Code - Nom du cours",
-    description: "Site de référence du cours de x",
-    nomUrl: "depinfo-gabarit"
+    nom: "420-4A4-EM - Réseaux de neurones et sciences",
+    description: "Site de référence du cours de réseaux de neurones pour le programme de sciences de la nature",
+    nomUrl: "420-4A4"
 }
 
 module.exports = config;
